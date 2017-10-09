f812b6e057dbd7a10e72606ac3f7e71a898e1c19
Thomas van Dam <t.vandam@wearetriple.com>
2017-10-09 14:37:44 +0200

    Replace pointless test with a first basic set of tests

--- test/Dash.test.js
+++ test/Dash.test.js
@@ -2,7 +2,22 @@ import '@meisterplayer/meister-mock';
 import Dash from '../src/js/Dash';
 
 const PLUGIN_NAME = 'Dash';
-const SUPPORTED_TYPES = ['dash', 'mpd'];
+const TEST_TYPES_DEFAULT = [
+    { type: 'mpd', supported: true },
+    { type: 'dash', supported: true },
+    { type: 'smooth', supported: false },
+    { type: 'mss', supported: false },
+    { type: 'random', supported: false },
+];
+
+const TEST_TYPES_SMOOTH = TEST_TYPES_DEFAULT.map(({ type, supported }) => {
+    let newSupported = supported;
+    if (type === 'smooth' || type === 'mss') {
+        newSupported = true;
+    }
+
+    return { type, supported: newSupported };
+});
 
 describe('Dash class', () => {
     test(`pluginName should be ${PLUGIN_NAME}`, () => {
@@ -15,10 +30,95 @@ describe('Dash class', () => {
     });
 });
 
-describe('The rest of the test suite', () => {
-    test('It should be written', () => {
-        const test = { testsWritten: false };
+describe('Dash', () => {
+    let getPlayerByTypeMock;
+    let triggerMock;
+    let oneMock;
+    let meisterInstanceMock;
+    let dashConfigMock;
 
-        expect(test).toEqual({ testsWritten: true });
+    let PLAYER_MOCK;
+    let ITEM_MOCK;
+
+    const SRC_STUB = 'https://stream_example.com/dash';
+    const TYPE_STUB = 'dash';
+
+    let dash;
+
+    beforeEach(() => {
+        getPlayerByTypeMock = jest.fn();
+        triggerMock = jest.fn();
+        oneMock = jest.fn();
+
+        meisterInstanceMock = {
+            getPlayerByType: getPlayerByTypeMock,
+            trigger: triggerMock,
+        };
+
+        dashConfigMock = {};
+        PLAYER_MOCK = {};
+        ITEM_MOCK = { type: TYPE_STUB, src: SRC_STUB };
+
+        global.Meister.MediaPlugin.prototype.one = oneMock;
+
+        dash = new Dash(dashConfigMock, meisterInstanceMock);
+    });
+
+    describe('isItemSupported', () => {
+        describe('without mediaSource default configuration', () => {
+            TEST_TYPES_DEFAULT.forEach((typeObject) => {
+                test(`It should resolve ${typeObject.type} items with supported: false`, async () => {
+                    const ITEM_STUB = { src: SRC_STUB, type: typeObject.type };
+
+                    const result = dash.isItemSupported(ITEM_STUB);
+                    await expect(result).resolves.toEqual({
+                        supported: false,
+                    });
+                });
+            });
+        });
+
+        describe('with mediaSource', () => {
+            beforeEach(() => {
+                global.MediaSource = true;
+            });
+
+            afterEach(() => {
+                delete global.MediaSource;
+            });
+
+            describe('default configuration', () => {
+                TEST_TYPES_DEFAULT.forEach((typeObject) => {
+                    test(`It should resolve ${typeObject.type} items with supported: ${typeObject.supported}`, async () => {
+                        const ITEM_STUB = { src: SRC_STUB, type: typeObject.type };
+
+                        const result = dash.isItemSupported(ITEM_STUB);
+                        await expect(result).resolves.toEqual({
+                            supported: typeObject.supported,
+                        });
+                    });
+                });
+            });
+
+            describe('Smooth enabled', () => {
+                beforeEach(() => {
+                    dashConfigMock = {
+                        enableSmooth: true,
+                    };
+
+                    dash = new Dash(dashConfigMock, meisterInstanceMock);
+                });
+                TEST_TYPES_SMOOTH.forEach((typeObject) => {
+                    test(`It should resolve ${typeObject.type} items with supported: ${typeObject.supported}`, async () => {
+                        const ITEM_STUB = { src: SRC_STUB, type: typeObject.type };
+
+                        const result = dash.isItemSupported(ITEM_STUB);
+                        await expect(result).resolves.toEqual({
+                            supported: typeObject.supported,
+                        });
+                    });
+                });
+            });
+        });
     });
 });
